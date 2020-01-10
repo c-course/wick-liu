7df7dd7b57ad020b179f8726c5b6b6caf186ccd7
Wick Liu <wick.liu@outlook.com>
2020-01-10 21:28:03 +0800

    File copy
    Resolve #1

--- demo1.c
+++ demo1.c
@@ -1,4 +1,25 @@
+#include <fcntl.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/stat.h>
+
 int main()
 {
-	return 1;
+    char buf;
+    int fd = open("../out.txt", O_RDWR);
+    int wd = open("../in.txt", O_WRONLY | O_CREAT, S_IRWXU);
+
+    if (fd < 0) {
+        return fd;
+    }
+
+    if (wd < 0) {
+        return wd;
+    }
+
+    while (read(fd, &buf, 1) > 0) {
+        write(wd, &buf, 1);
+    }
+
+    return 0;
 }
